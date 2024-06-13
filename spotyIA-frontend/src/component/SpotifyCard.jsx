36c9d1bed7aefaf3ad1ff1bed95abdb00a3421d4
youssef-404 <eyoussef081@gmail.com>
2024-06-13 16:11:50 +0200

    finalize frontedn

--- spotyIA-frontend/src/component/SpotifyCard.jsx
+++ spotyIA-frontend/src/component/SpotifyCard.jsx
@@ -4,9 +4,10 @@ import songPLayer from "../assets/songPlayer.svg"
 export default function SpotifyCard({songInfo}){
 
     return(
+        <>
         <div className="relative flex min-w-1/3 flex-col justify-center overflow-hidden p-6 rounded-xl shadow-lg shadow-white/30">
             <div className="z-10 flex flex-col gap-4 justify-center items-center">
-                <div className="border border-white/20">
+                <div className="border border-white/20 w-72">
                     <img src={songInfo.trackCover} alt="track cover" />
                 </div>
                 <div className=" self-start">
@@ -18,6 +19,7 @@ export default function SpotifyCard({songInfo}){
                 </div>
             </div>
       </div>
+    </>
 
     )
 }
